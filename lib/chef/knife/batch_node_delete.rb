842a992c5d6a3aca88d4b738674386fb8989c7d1
Joshua Riojas <joshuariojas.psu@gmail.com>
2022-04-06 23:52:13 -0400

    Add boilerplate

--- lib/chef/knife/batch_node_delete.rb
+++ lib/chef/knife/batch_node_delete.rb
@@ -1,5 +1,5 @@
 require 'chef/knife'
-require 'helpers/batch_node_base'
+require_relative 'helpers/batch_node_base'
 
 class Chef
   class Knife
@@ -7,19 +7,47 @@ class Chef
 
       include Chef::Knife::BatchNodeBase
 
-      banner 'knife batch node delete NODE_LIST (options)'
+      banner 'knife batch node delete [NODE_NAME [NODE_NAME]] (options)'
       category 'batch'
 
       def run
         STDOUT.sync = STDERR.sync = true
 
-        if @named_args.empty?
-          ui.error "Please specify the node list path. e.g-  'knife batch node delete <path>'"
-          exit 1
+        config[:format] = 'json'
+
+        api   = Chef::ServerAPI.new
+        nodes = batch_args
+        resp  = {
+          :errors => false,
+          :items  => {
+            :nodes   => [],
+            :clients => []
+          }
+        }
+
+        nodes.each do |node_name|
+          node_name = node_name.strip
+          next if node_name == ''
+
+          begin
+
+          rescue StandardError => e
+
+          end
+
+          unless config[:include_clients]
+            ui.output(resp)
+            return
+          end
+
+          begin
+
+          rescue StandardError => e
+
+          end
+
         end
 
-        file_path = @named_args.first
-        api_endpoint = 'nodes/'
       end
     end
   end
